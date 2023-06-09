8b9bd43d8f83733af58c0a01787ba3a47ba4e62f
YurGoV <yurgov@gmail.com>
2023-06-09 10:51:58 +0300

    ADD: catppuccin colorcsheme, now work with DankMono Nerd Fonts

--- highlights/duskfox.lua
+++ highlights/duskfox.lua
@@ -1,3 +1,5 @@
-return { -- a table of overrides/changes to the duskfox theme
-  Normal = { bg = "#000000" },
+return {
+  -- a table of overrides/changes to the duskfox theme
+  -- Normal = { bg = "#000000" },
+  -- Normal = { bg = "rgb(77, 0, 0)" },
 }
--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,55 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.note-taking.neorg" },
+  -- {
+  --   "neorg",
+  --   run = ":Neorg sync-parsers", -- This is the important bit!
+  --   config = function()
+  --     require("neorg").setup {
+  --       -- configuration here
+  --     }
+  --   end,
+  -- },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        nvimtree = false,
+        ts_rainbow = false,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        which_key = true,
+      },
+      styles = {
+        -- comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
 }
